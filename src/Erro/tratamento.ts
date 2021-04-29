4ea4919d35e066436971cf2ad51f4d9ee99b9a6c
Walaks Alves <walaksalves@wizsolucoes.com.br>
2021-04-29 01:16:14 -0300

    Ajuste de url base

--- src/Erro/tratamento.ts
+++ src/Erro/tratamento.ts
@@ -1,7 +1,7 @@
 export const tratamentoErro = (status : number, mensagem : string[]) => {
     if(status === 401)
     {
-      window.location.href = `http://localhost:3000/login`;
+      window.location.href = `https://grannishi.azurewebsites.net/login`;
       return null;
     }
     else if(status === 400 || status === 500)
